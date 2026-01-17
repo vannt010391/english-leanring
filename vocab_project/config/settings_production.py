8e57927c5039c492389e5d14ff546e2a19b20871
vannt010391 <vannt.sptb@gmail.com>
2026-01-17 13:06:33 +0700

    Add production settings file for deployment

---
+++ vocab_project/config/settings_production.py
@@ -0,0 +1,25 @@
+"""
+Production settings for VocabMaster project.
+"""
+from .settings import *
+
+# Production settings
+DEBUG = False
+ALLOWED_HOSTS = ['english.iamstudying.tech', 'www.english.iamstudying.tech', '178.128.58.51', 'localhost', '127.0.0.1']
+
+# Security settings
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+
+# Static files
+STATIC_ROOT = '/var/www/vocabmaster/vocab_project/staticfiles'
+STATIC_URL = '/static/'
+
+# Media files
+MEDIA_ROOT = '/var/www/vocabmaster/vocab_project/media'
+MEDIA_URL = '/media/'
+
+# Whitenoise for static files
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
